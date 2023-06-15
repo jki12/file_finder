e53d9b66a28e9fbf10b025bb93208425e9333c7e
jki12 <sjak0227@naver.com>
2023-06-15 20:17:33 +0900

    :)

--- src/Finder.java
+++ src/Finder.java
@@ -3,6 +3,9 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Finder extends JPanel {
     private static final String DEFAULT_PATH = "C:\\";
@@ -16,11 +19,16 @@ public class Finder extends JPanel {
 
     private JPanel optionPanel;
     private JCheckBox strictCheckBox;
+    private JCheckBox ignoreCaseCheckBox;
     private JFileChooser dirChooser;
     private JTextField pathField; // always disabled.
-    private String currentPath = DEFAULT_PATH;
     private JTextField depthField;
+
+    private String findPath = DEFAULT_PATH;
     private int depth = DEFAULT_DEPTH;
+
+    private StringBuilder sb = new StringBuilder();
+    private ArrayList<String> foundFiles = new ArrayList<>();
 
     public Finder() {
         this.setLayout(new BorderLayout());
@@ -32,10 +40,11 @@ public class Finder extends JPanel {
         dirChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         pathField = new JTextField(30);
         strictCheckBox = new JCheckBox("strict mode");
+        ignoreCaseCheckBox = new JCheckBox("ignore case");
 
-        depthField = new JTextField(String.valueOf(depth), 30);
+        depthField = new JTextField(String.valueOf(DEFAULT_DEPTH), 30);
 
-        pathField.setText(currentPath);
+        pathField.setText(DEFAULT_PATH);
         pathField.setEnabled(false);
 
         var btn = new JButton("select path");
@@ -43,15 +52,16 @@ public class Finder extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 dirChooser.showDialog(optionPanel, "select");
-                currentPath = dirChooser.getSelectedFile().getPath();
+                findPath = dirChooser.getSelectedFile().getPath();
 
-                pathField.setText(currentPath);
+                pathField.setText(findPath);
             }
         });
 
         optionPanel.add(pathField);
         optionPanel.add(btn); // click -> open file chooser
         optionPanel.add(strictCheckBox);
+        optionPanel.add(ignoreCaseCheckBox);
         optionPanel.add(new JLabel("depth"));
         optionPanel.add(depthField);
 
@@ -63,9 +73,13 @@ public class Finder extends JPanel {
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("current setting");
+                if (!isValid(depthField.getText())) return; // showing popup.
 
-                System.out.println(searchField.getText() + " " + strictCheckBox.isSelected() + " ");
+                depth = Integer.parseInt(depthField.getText()); // valid data.
+
+                search(searchField.getText());
+
+                System.out.println("done : " + foundFiles.size());
             }
         });
 
@@ -74,6 +88,10 @@ public class Finder extends JPanel {
 
         this.add(searchPanel);
         this.add(optionPanel, BorderLayout.AFTER_LAST_LINE);
+    }
+
+    public ArrayList<String> getFoundFiles() {
+        return foundFiles;
     }
 
     private boolean isValid(String s) {
@@ -88,13 +106,14 @@ public class Finder extends JPanel {
         return (MIN_DEPTH <= n && n <= MAX_DEPTH);
     }
     
-    private void search() {
+    private void search(String fileName) { // 찾고자 하는 파일명.
+        foundFiles.clear();
         searchButton.setEnabled(false);
         
         try {
-            File file = new File(currentPath);
+            File file = new File(findPath);
             
-            dfs(file, depth);
+            dfs(file, fileName, 0);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -102,7 +121,28 @@ public class Finder extends JPanel {
         searchButton.setEnabled(true);
     }
 
-    private void dfs(File file, int depth)
+    private boolean compare(final String s1, final String s2, boolean ignoreCase) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < s1.length(); ++i) {
+            if (ignoreCase) {
+                if ((s1.charAt(i) | ' ') != (s2.charAt(i) | ' ')) {
+                    return false;
+                }
+            }
+            else {
+                if (s1.charAt(i) != s2.charAt(i)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public void dfs(File file, final String fileName, int depth)
     {
         assert (file.isDirectory());
 
@@ -112,26 +152,37 @@ public class Finder extends JPanel {
 
         var list = file.list();
 
+        if (list == null) {
+            return;
+        }
+
         for (int i = 0; i < list.length; ++i) {
+            String path = file.getPath() + "\\" + list[i]; // current path.
 
-            String path = file.getPath() + "\\" + list[i];
-
-            // strict ver, normal ver
-            if (.equals(list[i])) {
+            if (compare(removeFileExtension(list[i]), removeFileExtension(fileName), false)) { // temp code.
                 foundFiles.add(path);
             }
 
             boolean isDir = Files.isDirectory(Paths.get(path));
 
             if (isDir) {
-                dfs(new File(path), depth + 1);
+                dfs(new File(path), fileName, depth + 1);
             }
         }
-
-        // System.out.println(getFormatter(depth, false, true)); // 가독성 높이려고 넣었음.
     }
 
+    private String removeFileExtension(String fileName) { // test..
+        int index = fileName.lastIndexOf('.');
 
+        if (index == -1) {
+            return fileName;
+        }
 
+        sb.setLength(0);
+        for (int i = 0; i < index; ++i) {
+            sb.append(fileName.charAt(i));
+        }
 
+        return sb.toString();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        var path = new File("C:\\Users\\sjak0\\Desktop\\algorithm");
+        var fileName = "b1697.java";
+
+
 
         var f = new Frame();
         f.setSize(1000, 1000); // tnwjd
